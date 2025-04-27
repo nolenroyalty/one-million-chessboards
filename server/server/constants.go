a2874ea2f55d6ca1ef4a203aa7e200d0729b1f82
Nolen <nolen.royalty@gmail.com>
2025-04-27 04:02:10 -0400

    tweak view radius

--- server/server/constants.go
+++ server/server/constants.go
@@ -7,10 +7,24 @@ import (
 	"github.com/klauspost/compress/zstd"
 )
 
+// nroyalty: I'm pretty tired, but I *think* it is just safe to say that
+// zone size can be 50. We consider everything in the client's current zone,
+// plus the zone to their left and right (and up and down, etc). Given that,
+// we should always be including every move in the client's snapshot window.
+//
+// We could probably even shrink zone count to 40 and still get away with things
+// to be honest.
+//
+// In fact, I think we clearly can. If we ignore the client's current zone (because
+// they're on the far left or right, etc) we still will have a 40 square buffer
+// and their max half view radius is 35 or 36 depending on how you count.
+//
+// if we get weird reports of missing moves we can investigate further?
+
 const (
 	BOARD_SIZE                  = 8000
 	SINGLE_BOARD_SIZE           = 8
-	ZONE_SIZE                   = 80
+	ZONE_SIZE                   = 40
 	ZONE_COUNT                  = BOARD_SIZE / ZONE_SIZE
 	TOTAL_ZONES                 = ZONE_COUNT * ZONE_COUNT
 	VIEW_RADIUS                 = 47
